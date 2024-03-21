648454c21ad5573d4d4271e9ad25b21f5a5db2a2
huappy <111156347+huappy@users.noreply.github.com>
2024-03-21 18:45:01 -0500

    Grey out dropdowns and text
    Made it so using the left hand search parameters change the right hand side's text to grey and using the left hand search box changes the right hand's text and dropdowns to grey in order to show that they are disabled.

--- src/tstables/src/checkDropdownInput.ts
+++ src/tstables/src/checkDropdownInput.ts
@@ -1,16 +1,19 @@
-function checkDropdownInput(dropdown: string, textbox: string)
+function checkDropdownInput(dropdown: string, div: string, textbox: string)
 {
     var val = (<HTMLInputElement>document.getElementById(dropdown)!).value
+    var div_background = (<HTMLInputElement>document.getElementById(div)!)
     if (val == 'All')
     {
-        (<HTMLInputElement>document.getElementById(textbox)).disabled = false
-        
+        (<HTMLInputElement>document.getElementById(textbox)).disabled = false;
+        (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#f2f2f2";
+        div_background.style.color = "#555555"
         
     }
     else
     {
-        (<HTMLInputElement>document.getElementById(textbox)).disabled = true
-
+        (<HTMLInputElement>document.getElementById(textbox)).disabled = true;
+        (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#e1e1e1";
+        div_background.style.color = "grey"
     }
     
 }
