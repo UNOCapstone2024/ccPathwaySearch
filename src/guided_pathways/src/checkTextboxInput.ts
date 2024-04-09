b6fcf43f45df0613fa342b2566c68482ed1aaa2a
Zach Simms <zachsimms97@gmail.com>
2024-04-09 13:43:12 -0500

    docs(guided_pathways): add function doc strings

--- src/guided_pathways/src/checkTextboxInput.ts
+++ src/guided_pathways/src/checkTextboxInput.ts
@@ -1,4 +1,15 @@
-//checks input 
+/**
+ * Adjusts the properties of multiple elements based on the input length in a specified textbox.
+ *
+ * This function assesses the input value's length in a textbox. If the input length is less than 1,
+ * it enables and modifies the background color of each element in the provided list. Additionally,
+ * it adjusts the color of a specified div element. If the textbox contains any input, it disables
+ * the elements in the list, changes their background color, and modifies the div's color to grey.
+ *
+ * @param {string} id - The ID of the textbox HTML element to be checked.
+ * @param {string} div - The ID of the div HTML element whose color will be modified.
+ * @param {Array<string>} list - An array of IDs for the HTML elements to be enabled/disabled and styled.
+ */ 
 function checkTextboxInput(id: string, div:string, list: Array<string>)
 {
     var val = (<HTMLInputElement>document.getElementById(id)!).value
