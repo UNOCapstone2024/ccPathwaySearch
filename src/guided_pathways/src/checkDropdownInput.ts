d7e9b89acf3b743e0a02aae70c2ddbf016cd0acb
huappy <111156347+huappy@users.noreply.github.com>
2024-04-29 16:21:21 -0500

    Dark Mode Colors Change with Disable Functions
    Dark Mode Colors Change with Disable Functions.
    TODO: Integrate into the CSS

--- src/guided_pathways/src/checkDropdownInput.ts
+++ src/guided_pathways/src/checkDropdownInput.ts
@@ -4,7 +4,9 @@ function checkDropdownInput(dropdown: Array<string>, div: string, textbox: strin
     var div_background = (<HTMLInputElement>document.getElementById(div)!)
     var bgColor = div_background.style.backgroundColor
     var disable_items = false
+    const theme = (document.getElementsByTagName("html")[0]).getAttribute("data-bs-theme")
     var val;
+
     for(var i = 0; i < dropdown.length; i++)
     {
         val = (<HTMLInputElement>document.getElementById(dropdown[i])!).value
@@ -21,21 +23,37 @@ function checkDropdownInput(dropdown: Array<string>, div: string, textbox: strin
             disable_items = true;
             break
         }
-        console.log(disable_items)
     }
 
     if (disable_items == false)
     {
         (<HTMLInputElement>document.getElementById(textbox)).disabled = false;
-        (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#f2f2f2";
-        div_background.style.color = "#555555"
+
+        if(theme == "light"){
+            (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#f2f2f2";
+            div_background.style.color = "#555555"
+        }
+        else if(theme == "dark"){
+            (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#333333";
+            div_background.style.color = "#f2f2f2"
+        }
+        
         
     }
     else
     {
         (<HTMLInputElement>document.getElementById(textbox)).disabled = true;
-        (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#e1e1e1";
-        div_background.style.color = "grey"
+
+        if(theme == "light"){
+            (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#e1e1e1";
+            div_background.style.color = "grey"
+        }
+
+        else if(theme == "dark"){
+            (<HTMLInputElement>document.getElementById(textbox)).style.backgroundColor = "#555555";
+            div_background.style.color = "dark grey"
+        }
+
     }
     
 }
